a907b8420753f2f28c16ffceb4b968262c2ec814
xprimen <engga.muse@gmail.com>
2024-08-23 17:14:03 +0700

    Update Role di Comp UserInfo

--- src/components/UserInfo/index.tsx
+++ src/components/UserInfo/index.tsx
@@ -19,7 +19,7 @@ const UserInfo = ({ userdata }: Props) => {
         <h2 className="font-bold text-xl">{userdata?.nama}</h2>
         <p className="text-slate-500 text-md">@{userdata?.username}</p>
         <p className="text-green-600 text-md">
-          {userdata?.roleId === "2" ? "PIC Wilayah" : "PIC Kotak"}
+          {userdata?.role === "2" ? "PIC Wilayah" : "PIC Kotak"}
         </p>
       </div>
       {/* <LucidePencil className="mr-4" /> */}
